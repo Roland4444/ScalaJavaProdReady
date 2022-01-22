9e4e0b9d8fa963662b397d88ff04b04cb7907f22
Roland4444 <turbopollux@rambler.ru>
2022-01-22 15:21:53 +0400

    first commit

---
+++ src/main/scala/ch/roland/KeyValue.scala
@@ -0,0 +1,6 @@
+package ch.roland
+
+/**
+ * @author ${user.name}
+ */
+case class KeyValue(key: String, Value: Object)
---
+++ src/test/scala/samples/junit.scala
@@ -0,0 +1,17 @@
+package samples
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(2==2)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
